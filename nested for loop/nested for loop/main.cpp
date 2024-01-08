385a074e8010dc568e1484d212caf2b1bab05a0c
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 22:53:04 +0800

    Add files via upload

---
+++ nested for loop/nested for loop/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  nested for loop
+//
+//  Created by Nisha Ramprasath on 31/10/23.
+//
+//
+////////////////////////////
+//#include <iostream>
+//using namespace std;
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    for (int i=1;i<=5;i++){
+//        for(int j=1;j<=5;j++){
+//            cout<<j<<" ";
+//        }
+//        cout<<endl;
+//    }    return 0;
+//}
+/////////////////
+///2d array
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    for (int i=1;i<=3;i++){
+        for(int j=1;j<=3;j++){
+            cout<<"("<<i<<","<<j<<")";
+        }
+        cout<<endl;
+    }    return 0;
+}
